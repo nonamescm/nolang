43fd432db73d6460497ff90e6d0a062c105b0365
Murilo Menezes <snadragona12bionic@gmail.com>
2021-06-25 21:13:43 -0300

    add file eval

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,48 @@
 mod lexer;
 use lexer::Lexer;
-use std::io::{
-    stdout,
-    stdin,
-    Write,
+use std::{
+    io::{
+        stdout,
+        stdin,
+        Write,
+    },
+    env::args,
+    fs::read_to_string,
 };
 
-fn main() -> Result<(), std::io::Error> {
+type IoResult = Result<(), std::io::Error>;
+
+fn main() -> IoResult {
+    if args().collect::<Vec<String>>().len() < 2 {
+        repl()?;
+    } else {
+        interpret()?;
+    }
+    Ok(())
+}
+
+fn interpret() -> IoResult {
+    let mut arguments = args();
+    arguments.next();
+
+    for file in arguments {
+        println!("{}", file);
+        let lexer = Lexer::new(
+            read_to_string(file)?.chars().collect()
+        ).start();
+
+        if !lexer.is_empty() {
+            println!("[");
+            for element in lexer.iter() {
+                println!("  {:?},", element)
+            }
+            println!("]");
+        }
+    }
+    Ok(())
+}
+
+fn repl() -> IoResult {
     loop {
         print!("NoLang(REPL)> ");
         stdout().flush()?;
@@ -14,12 +50,14 @@ fn main() -> Result<(), std::io::Error> {
         let mut input = String::new();
         stdin().read_line(&mut input)?;
 
-        let mut lexer = Lexer::new(input.chars().collect());
+        let lexer = Lexer::new(input.chars().collect()).start();
 
-        println!("[");
-        for element in lexer.start().iter() {
-            println!("  {:?},", element)
+        if !lexer.is_empty() {
+            println!("[");
+            for element in lexer.iter() {
+                println!("  {:?},", element)
+            }
+            println!("]");
         }
-        println!("]");
     }
 }
